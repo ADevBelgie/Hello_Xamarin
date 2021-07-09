1b3de07ecf36406adcf4f00ea20ba93f7394f530
ADev <arthur.devresse@gmail.com>
2021-07-09 12:10:26 +0200

    Add project files.

---
+++ Hello_Xamarin/Hello_Xamarin/Models/Fruit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hello_Xamarin.Models
+{
+    public class Fruit
+    {
+        public string Name { get; set; }
+        public int Weight { get; set; }
+        public string Color { get; set; }
+        public decimal PriceKilo { get; set; }
+        public int Id { get; set; }
+        public string Species { get; set; }
+    }
+}
---
+++ Hello_Xamarin/Hello_Xamarin/ViewModels/FruitViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Hello_Xamarin.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Hello_Xamarin.ViewModels
+{
+    public class FruitViewModel
+    {
+        public Fruit Fruit { get; set; }
+        public string Klant { get; set; } = "Verkoper";
+        
+        public Command UpdateFruit { get; }
+        public FruitViewModel()
+        {
+            Fruit = new Fruit();
+            UpdateFruit = new Command(OnFruitUpdate);
+        }
+
+        private void OnFruitUpdate(object obj)
+        {
+            
+        }
+    }
+}
