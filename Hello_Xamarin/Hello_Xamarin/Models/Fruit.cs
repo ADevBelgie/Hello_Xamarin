82546c484d0a5442cfbfae3a656c99738e147c4b
ADev <arthur.devresse@gmail.com>
2021-07-12 08:50:05 +0200

    fruit

--- Hello_Xamarin/Hello_Xamarin/Models/Fruit.cs
+++ Hello_Xamarin/Hello_Xamarin/Models/Fruit.cs
@@ -6,11 +6,17 @@ namespace Hello_Xamarin.Models
 {
     public class Fruit
     {
+        public Fruit(string species, string image)
+        {
+            Species = species;
+            Image = image;
+        }
         public string Name { get; set; }
         public int Weight { get; set; }
         public string Color { get; set; }
         public decimal PriceKilo { get; set; }
         public int Id { get; set; }
         public string Species { get; set; }
+        public string Image { get; set; }
     }
 }
--- Hello_Xamarin/Hello_Xamarin/ViewModels/FruitViewModel.cs
+++ Hello_Xamarin/Hello_Xamarin/ViewModels/FruitViewModel.cs
@@ -8,13 +8,15 @@ namespace Hello_Xamarin.ViewModels
 {
     public class FruitViewModel
     {
-        public Fruit Fruit { get; set; }
+        public List<Fruit> Fruit { get; set; } = new List<Fruit>();
         public string Klant { get; set; } = "Verkoper";
         
         public Command UpdateFruit { get; }
         public FruitViewModel()
         {
-            Fruit = new Fruit();
+            Fruit.Add(new Fruit("Apple", "apple.jpg"));
+            Fruit.Add(new Fruit("Banana", "Banana.jpg"));
+            Fruit.Add(new Fruit("Pear", "pear.jpg"));
             UpdateFruit = new Command(OnFruitUpdate);
         }
 
