87fa35878798078c1575952dde7a4eb9d35adb1e
Matthijs Debacker <matthijsdebacker@hotmail.com>
2021-07-12 10:24:50 +0200

    Button Updates

--- Hello_Xamarin/Hello_Xamarin/ViewModels/FruitViewModel.cs
+++ Hello_Xamarin/Hello_Xamarin/ViewModels/FruitViewModel.cs
@@ -1,28 +1,47 @@
 ﻿using Hello_Xamarin.Models;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 using Xamarin.Forms;
 
 namespace Hello_Xamarin.ViewModels
 {
-    public class FruitViewModel
+    public class FruitViewModel : BaseViewModel
     {
-        public List<Fruit> Fruit { get; set; } = new List<Fruit>();
+        public ObservableCollection<Fruit> Fruit { get; set; } = new ObservableCollection<Fruit>();
         public string Klant { get; set; } = "Verkoper";
         
-        public Command UpdateFruit { get; }
+        public Command UpdateFruitCommand { get; }
+
+        public Command AddFruitCommand { get; }
+
+        public Command RemoveFruitCommand { get; }
+
         public FruitViewModel()
         {
             Fruit.Add(new Fruit("Apple", "apple.jpg"));
             Fruit.Add(new Fruit("Banana", "Banana.jpg"));
             Fruit.Add(new Fruit("Pear", "pear.jpg"));
-            UpdateFruit = new Command(OnFruitUpdate);
+
+            UpdateFruitCommand = new Command(OnFruitUpdate);
+            AddFruitCommand = new Command(OnAddFruit);
+            RemoveFruitCommand = new Command(OnRemoveFruit);
         }
 
         private void OnFruitUpdate(object obj)
         {
             
+        }
+
+        private void OnAddFruit()
+        {
+            Fruit.Add(new Fruit("Apple", "apple.jpg"));
+        }
+
+        private void OnRemoveFruit()
+        {
+            Fruit.RemoveAt(Fruit.Count - 1);
         }
     }
 }
